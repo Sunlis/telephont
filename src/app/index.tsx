5bd32d49b85d16326947dc4943d3081218e4b551
tablekat <katherine.hill.c9@gmail.com>
2020-05-30 15:00:34 -0700

    learning git again

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <span>waddup</span>
+        <span>re-learning git time pogg champ</span>
       </div>
     );
   }
